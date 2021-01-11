a54f6d733bb57a4e87b0371cabd8eed5625dc956
Bjorn Timmerhuis <bjorn@MacBook-Pro.local>
2021-01-11 17:37:27 +0100

    added Pallete component

---
+++ src/pallete.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Pallete extends Component {
+  render() {
+    return (
+      <div>
+        {/* Navbar will go here */}
+        <div>{/* a bunch of colorboxes to be rendered here */}</div>
+        {/* Footer will go here */}
+      </div>
+    );
+  }
+}
